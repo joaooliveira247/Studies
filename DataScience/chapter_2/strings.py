a91b281225368430ec61e08fa1c931261b8f06a0
joaooliveira247 <joaooliveira247@gmail.com>
2024-03-04 19:10:23 -0300

    :sparkles: add strings

---
+++ DataScience/chapter_2/strings.py
@@ -0,0 +1,40 @@
+from icecream.icecream import ic
+
+
+def main() -> None:
+    # in python we can express strings with single quotes ou double quotes
+    # in another languages single quotes express a char
+    single_quoted_string: str = "data science"
+    double_quoted_string: str = "data science"
+
+    ic(single_quoted_string)
+    ic(double_quoted_string)
+
+    tab_string: str = "\t"
+    ic(len(tab_string))
+
+    # if you use r-strings you don't need use escape \
+    not_tab_string: str = r"\t"
+    ic(len(not_tab_string))
+    ic(not_tab_string)
+
+    multi_line_string: str = """
+    "Hope" is the thing with feathers
+    That perches in the soul
+    And sings the tune without the words
+    And never stops at all,
+
+    - Emily Dickinson, Hope is the Thing with Feathers
+    """
+
+    print(multi_line_string)
+
+    # we can concat string by differents ways:
+    first_name, last_name = "Joel", "Grus"
+    ic(first_name + " " + last_name)
+    ic("{0} {1}".format(first_name, last_name))
+    ic(f"{first_name} {last_name}")
+
+
+if __name__ == "__main__":
+    main()
