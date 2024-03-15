92e13233844898d3070e4ad3ab72764670fbb1fb
joaooliveira247 <joaooliveira247@gmail.com>
2024-03-15 11:11:51 -0300

    :sparkles: add third_dag

---
+++ airflow/proj_1/dags/third_dag.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+
+dag = DAG(
+    "third_dag",
+    description="My third DAG",
+    schedule_interval=None,
+    start_date=datetime(2024, 3, 10),
+    catchup=False,  # if cathup airflow will execute passed dates diffente now
+)
+
+task_1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
+task_2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
+task_3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
+
+# this dag task will run pararel before
+[task_1, task_2] >> task_3
