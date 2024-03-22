00af77937f8154c274f5968db0b3ac978534bfe3
joaooliveira247 <joaooliveira247@gmail.com>
2024-03-22 12:00:42 -0300

    :sparkles: use patch

--- tests/collections/my_collections/livros.py
+++ tests/collections/my_collections/livros.py
@@ -1,2 +1,13 @@
+from __future__ import annotations
+
+
 def consultar_livros(author: str) -> str:
+    dados = preparar_dados_para_requisicao(author)
+    obter_url("https://buscador", dados)
     return ""
+
+
+def preparar_dados_para_requisicao(author): ...
+
+
+def obter_url(url, dados): ...
--- tests/collections/tests/test_livros.py
+++ tests/collections/tests/test_livros.py
@@ -1,6 +1,24 @@
 from my_collections.livros import consultar_livros
+from unittest.mock import patch
 
 
 def test_consultar_livros_retorna_formato_string() -> None:
     result: str = consultar_livros("Agatha Christie")
     assert isinstance(result, str) is True
+
+
+def test_consultar_livros_chama_preparar_dados_para_uma_vez_e_com_os_mesmos_parametros_de_consultar_livros():
+    with patch("my_collections.livros.preparar_dados_para_requisicao") as duble:
+        consultar_livros("Agatha Christie")
+        duble.assert_called_once_with("Agatha Christie")
+
+
+def test_consultar_livros_chama_obter_url_usando_parametro_retorno_preparar_dados_requisicao():
+    with patch(
+        "my_collections.livros.preparar_dados_para_requisicao"
+    ) as duble_preparar:
+        dados = {"author": "Agatha Christie"}
+        duble_preparar.return_value = dados
+        with patch("my_collections.livros.obter_url") as duble_obter_url:
+            consultar_livros("Agatha Christie")
+            duble_obter_url.assert_called_once_with("https://buscador", dados)
