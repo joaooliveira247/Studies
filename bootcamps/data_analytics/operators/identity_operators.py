1742a1e056952c6a44bbfa9e48ff3eb5238100e0
joaooliveira247 <joaooliveira247@gmail.com>
2024-04-06 11:17:27 -0300

    :sparkles: add identity_operators.py

---
+++ bootcamps/data_analytics/operators/identity_operators.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    some_name = "name"
+    other_name = some_name
+
+    print(some_name is other_name)
+    print(some_name is not other_name)
+
+
+if __name__ == "__main__":
+    main()
