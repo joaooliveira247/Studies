2c38db5d698645bcd839a0c5dd13052fe5b1966c
joaooliveira247 <joaooliveira247@gmail.com>
2024-04-06 09:19:29 -0300

    :sparkles: add atribuition_operators.py

---
+++ bootcamps/data_analytics/operators/atribuition_operatos.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    # simple atribuition
+    some_name = 500
+
+    # addition atribuition(we can do it with all aritmetic operators)
+    some_name += 200
+
+
+if __name__ == "__main__":
+    main()
