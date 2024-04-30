9d166e14b36f9304edb121df87e78d2dbffaf4c6
joaooliveira247 <joaooliveira247@gmail.com>
2024-04-30 16:48:26 -0300

    :sparkles: end decorators

--- bootcamps/python_ai_backend_dev/advanced/decorator.py
+++ bootcamps/python_ai_backend_dev/advanced/decorator.py
@@ -1,19 +1,39 @@
+import functools
+
+
 def my_decorator(func: callable) -> None:
-    def env():
+    def env(*args, **kwargs):
         print("do something")
-        func()
+        func(*args, **kwargs)
         print("do something")
 
     return env
 
 
 @my_decorator
-def hello_world() -> None:
+def hello_world(name: str) -> None:
     print("Hello, world")
 
 
+def double(func: callable) -> callable:
+    @functools.wraps(func)
+    def env(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result
+
+    return env
+
+
+@double
+def learning(tec: str) -> str:
+    print(f"I'm learning {tec}")
+    return tec.upper()
+
+
 def main() -> None:
-    hello_world()
+    # hello_world("Some name")
+    print(learning("Python"))
+    print(learning.__name__)
 
 
 if __name__ == "__main__":
