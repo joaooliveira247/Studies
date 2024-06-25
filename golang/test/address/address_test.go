575e7b3f746b019b10dd020e57744b8e51afea4e
joaooliveira247 <joaooliveira247@gmail.com>
2024-06-25 07:11:11 -0300

    :sparkles: add tests with structs

--- golang/test/address/address_test.go
+++ golang/test/address/address_test.go
@@ -2,11 +2,43 @@ package address
 
 import "testing"
 
+type TestCase struct {
+	input  string
+	output string
+}
+
 func TestAddress(t *testing.T) {
 	testAddress := "Avenida Brasil 1507"
 	result := AddressType(testAddress)
 
 	if result != "Avenida" {
 		t.Error("Wrong type return.")
+	}
+}
+
+func TestAddressWithStruct(t *testing.T) {
+	testCases := []TestCase{
+		{
+			"Rua das flores", "Rua",
+		},
+		{
+			"Avenida Brasil 1507", "Avenida",
+		},
+		{
+			"Estrada da posse", "Estrada",
+		},
+		{
+			"Rodovia amaral peixoto", "Rodovia",
+		},
+		{
+			"Travessa cardoso", "Pattern not found",
+		},
+	}
+
+	for _, case_ := range testCases {
+		addressType := AddressType(case_.input)
+		if addressType != case_.output {
+			t.Errorf("Input type %s wait: %s got: %s", case_.input, case_.output, addressType)
+		}
 	}
 }
