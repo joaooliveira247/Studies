ed6c0ad2bc87e080816f698c53ceda93ae32351c
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-02 17:51:17 -0300

    :sparkles: add users controller

---
+++ golang/dev_book/api/src/controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func CreateUser(w http.ResponseWriter, t *http.Request) {
+	w.Write([]byte("Not implemented"))
+}
+
+func GetUsers(w http.ResponseWriter, t *http.Request) {
+	w.Write([]byte("Not implemented"))
+}
+
+func GetUserbyId(w http.ResponseWriter, t *http.Request) {
+	w.Write([]byte("Not implemented"))
+}
+
+func UpdateUser(w http.ResponseWriter, t *http.Request) {
+	w.Write([]byte("Not implemented"))
+}
+
+func DeleteUser(w http.ResponseWriter, t *http.Request) {
+	w.Write([]byte("Not implemented"))
+}
