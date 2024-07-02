2aa1645906333ecab0ad3681e34aa84c9b63d576
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-02 17:52:26 -0300

    :sparkles: add users router

---
+++ golang/dev_book/api/src/router/users.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var userRoutes = []Route{
+	{
+		URI:     "/users",
+		Method:  http.MethodPost,
+		Func:    controllers.CreateUser,
+		AuthReq: false,
+	},
+	{
+		URI:     "/users",
+		Method:  http.MethodGet,
+		Func:    controllers.GetUsers,
+		AuthReq: false,
+	},
+	{
+		URI:     "/users/{userId}",
+		Method:  http.MethodGet,
+		Func:    controllers.GetUserbyId,
+		AuthReq: false,
+	},
+	{
+		URI:     "/users/{userID}",
+		Method:  http.MethodPut,
+		Func:    controllers.UpdateUser,
+		AuthReq: false,
+	},
+	{
+		URI:     "/users/{userID}",
+		Method:  http.MethodDelete,
+		Func:    controllers.DeleteUser,
+		AuthReq: false,
+	},
+}
