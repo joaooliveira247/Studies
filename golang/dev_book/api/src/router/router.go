3fa2c56c74ccdf72f419faa7ea1ca7d27c5b4920
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-02 17:51:41 -0300

    :sparkles: add router struct

--- golang/dev_book/api/src/router/router.go
+++ golang/dev_book/api/src/router/router.go
@@ -1,6 +1,17 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	URI     string
+	Method  string
+	Func    func(http.ResponseWriter, *http.Request)
+	AuthReq bool
+}
 
 func GenRouter() *mux.Router {
 	return mux.NewRouter()
