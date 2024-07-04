1648b7914b44733e3ea45f8af7951b81d6e04522
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-04 18:50:46 -0300

    :sparkles: add method Create

--- golang/dev_book/api/src/repositories/users.go
+++ golang/dev_book/api/src/repositories/users.go
@@ -13,6 +13,24 @@ func NewUserRepository(db *sql.DB) *Users {
 	return &Users{db}
 }
 
-func (u Users) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository Users) Create(user models.User) (uint64, error) {
+	statement, err := repository.db.Prepare(
+		"INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?);",
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(user.Name, user.UserName, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastInsertedID), nil
 }
