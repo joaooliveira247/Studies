54fdf6828c5f8ff611b7e7e8a36630c29594009d
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-04 17:17:46 -0300

    :sparkles: add GetConnection func

---
+++ golang/dev_book/api/src/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
+)
+
+func GetConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.ConnectionStringDB)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
