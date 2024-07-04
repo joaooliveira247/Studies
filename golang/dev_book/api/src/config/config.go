28af8d3d58c26b4b6f87c0051be1934d033ec1b8
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-04 16:27:29 -0300

    :sparkles: add LoadEnv

---
+++ golang/dev_book/api/src/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// MariaDB connection string
+	ConnectionStringDB = ""
+	// API running port
+	Port = 0
+)
+
+// Load .env
+func LoadEnv() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 9000
+	}
+
+	ConnectionStringDB = fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+}
