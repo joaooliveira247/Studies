8b1b2b076df694ba3a29292f674615be124ee604
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-04 18:53:19 -0300

    :recycle: add return id when user is insert

--- golang/dev_book/api/src/controllers/users.go
+++ golang/dev_book/api/src/controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"api/src/models"
 	"api/src/repositories"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -30,7 +31,11 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	repository := repositories.NewUserRepository(db)
-	repository.Create(user)
+	userID, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(fmt.Sprintf("%d", userID)))
 }
 
 func GetUsers(w http.ResponseWriter, t *http.Request) {
