535f172deae5c32707f8647ef9407ec149552abd
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-05 16:26:54 -0300

    :sparkles: add cli

---
+++ golang/dev_book/api/src/cli/cli.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"api/src/config"
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/urfave/cli/v2"
+)
+
+func GenApp() *cli.App {
+	app := &cli.App{
+		Name:  "API CLI",
+		Usage: "CLI to admin API",
+		Commands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "Start API",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println("Running API")
+					r := router.GenRouter()
+					log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
+					return nil
+				},
+			},
+			{
+				Name:  "create-table",
+				Usage: "Create all tables in database",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println("Create table")
+					return nil
+				},
+			},
+		},
+	}
+	return app
+}
