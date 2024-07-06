f7c18231f8953e7766fa49dd8bf346686f03d422
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-06 17:05:02 -0300

    :sparkles: add response

---
+++ golang/dev_book/api/src/response/response.go
@@ -0,0 +1,21 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Erro(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{Error: err.Error()})
+}
