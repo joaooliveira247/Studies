40a39c2707bbecbfc85ed54e641e1988ef5b73a2
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-06 18:17:27 -0300

    :sparkles: add validate method

--- golang/dev_book/api/src/models/users.go
+++ golang/dev_book/api/src/models/users.go
@@ -1,12 +1,33 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type User struct {
-	ID uint `json:"id,omitempty"`
-	Name string `json:"name,omitempty"`
-	UserName string `json:"user_name,omitempty"`
-	Email string `json:"email,omitempty"`
-	Password string `json:"password,omitempty"`
+	ID        uint      `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	UserName  string    `json:"user_name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func (u *User) validate() error {
+
+	if u.Name == "" {
+		return errors.New("Field Cannot be empty.")
+	}
+	if u.UserName == "" {
+		return errors.New("Field Cannot be empty.")
+	}
+	if u.Email == "" {
+		return errors.New("Field Cannot be empty.")
+	}
+	if u.Password == "" {
+		return errors.New("Field Cannot be empty.")
+	}
+
+	return nil
 }
