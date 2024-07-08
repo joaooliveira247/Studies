fdd4e2179cf2fe2b54b1b3690a4bbc9f10540c9b
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-08 11:59:08 -0300

    :sparkles: implement UpdateUser

--- golang/dev_book/api/src/controllers/users.go
+++ golang/dev_book/api/src/controllers/users.go
@@ -29,7 +29,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err = user.Prepare(); err != nil {
+	if err = user.Prepare("create"); err != nil {
 		response.Erro(w, http.StatusBadRequest, err)
 		return
 	}
@@ -75,7 +75,7 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 func GetUserbyId(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
-	userID, err := strconv.ParseUint(params["userId"], 10, 64)
+	userID, err := strconv.ParseUint(params["userID"], 10, 64)
 	if err != nil {
 		response.Erro(w, http.StatusBadRequest, err)
 		return
@@ -99,8 +99,46 @@ func GetUserbyId(w http.ResponseWriter, r *http.Request) {
 	response.JSON(w, http.StatusOK, user)
 }
 
-func UpdateUser(w http.ResponseWriter, t *http.Request) {
-	w.Write([]byte("Not implemented"))
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	userID, err := strconv.ParseUint(params["userID"], 10, 64)
+	if err != nil {
+		response.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		response.Erro(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		response.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = user.Prepare("edit"); err != nil {
+		response.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := db.GetConnection()
+	if err != nil {
+		response.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+
+	if err = repository.Update(userID, user); err != nil {
+		response.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	response.JSON(w, http.StatusNoContent, nil)
 }
 
 func DeleteUser(w http.ResponseWriter, t *http.Request) {
