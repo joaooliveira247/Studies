78cbef26c1ee87b212e8dd27e62fa9da8339bc9c
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-08 10:44:14 -0300

    :sparkles: add search method

--- golang/dev_book/api/src/repositories/users.go
+++ golang/dev_book/api/src/repositories/users.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type Users struct {
@@ -33,4 +34,41 @@ func (repository Users) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(lastInsertedID), nil
+}
+
+func (u Users) Search(filter string) ([]models.User, error) {
+	filter = fmt.Sprintf("%%%s%%", filter)
+
+	lines, err := u.db.Query(
+		`SELECT
+			id, name, user_name, email, created_at 
+		FROM 
+			users 
+		WHERE 
+		name LIKE ? or user_name LIKE ?;`,
+		filter,
+		filter,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer lines.Close()
+
+	var users []models.User
+
+	for lines.Next() {
+		var user models.User
+
+		if err = lines.Scan(
+			&user.ID, &user.Name, &user.UserName, &user.Email, &user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
