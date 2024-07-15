43f4a7302866a3cf520b863299ec6518ae4a678e
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-15 11:04:05 -0300

    :sparkles: add posts routes

---
+++ golang/dev_book/api/src/router/posts.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var postsRouters = []Route{
+	{
+		URI: "/posts/",
+		Method: http.MethodPost,
+		Func: controllers.CreatePost,
+		AuthReq: true,
+	},
+	{
+		URI: "/posts/",
+		Method: http.MethodGet,
+		Func: controllers.GetPosts,
+		AuthReq: true,
+	},
+	{
+		URI: "/posts/{postID}",
+		Method: http.MethodGet,
+		Func: controllers.GetPostByID,
+		AuthReq: true,
+	},
+	{
+		URI: "/posts/{postID}",
+		Method: http.MethodPut,
+		Func: controllers.UpdatePost,
+		AuthReq: true,
+	},
+	{
+		URI: "/posts/{postID}",
+		Method: http.MethodDelete,
+		Func: controllers.DeletePost,
+		AuthReq: true,
+	},
+}
