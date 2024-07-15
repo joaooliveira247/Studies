f5fd16c3bdacf316f93d8a73d81d6b9780f35c7b
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-15 10:57:24 -0300

    :sparkles: add const postsTable

--- golang/dev_book/api/src/db/create.go
+++ golang/dev_book/api/src/db/create.go
@@ -30,6 +30,21 @@ CREATE TABLE IF NOT EXISTS followers(
 );
 `
 
+const postsTable = `
+CREATE TABLE IF NOT EXISTS posts(
+	id INT AUTOINCREMENT NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	content TEXT NOT NULL,
+	author_id INT NOT NULL
+	FOREIGN KEY (author_id)
+	REFERENCES users(id)
+	ON DELETE CASCADE,
+	likes INT DEFAULT 0,
+	created_at DATE default current_timestamp(),
+	PRIMARY KEY (id)
+);
+`
+
 func CreateTables() {
 	conn, err := GetConnection()
 	if err != nil {
@@ -37,7 +52,7 @@ func CreateTables() {
 	}
 	defer conn.Close()
 	tables := []string{
-		userTable, followerTable,
+		userTable, followerTable, postsTable,
 	}
 	for _, table := range tables {
 		if _, err := conn.Query(table); err != nil {
