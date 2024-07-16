8d5f54353bf5ee522c4097ff6ddb7abde7e7d310
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-15 21:17:58 -0300

    :sparkles: add CreatePost

--- golang/dev_book/api/src/repositories/posts.go
+++ golang/dev_book/api/src/repositories/posts.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "database/sql"
+import (
+	"api/src/models"
+	"database/sql"
+)
 
 type Posts struct {
 	db *sql.DB
@@ -8,4 +11,27 @@ type Posts struct {
 
 func NewPostsRepository(db *sql.DB) *Posts {
 	return &Posts{db}
+}
+
+func (p Posts) CreatePost(post models.Posts) (uint64, error) {
+	statement, err := p.db.Prepare(
+		`INSERT INTO 
+			posts (title, content, author_id)
+		VALUSE (?, ?, ?);
+		`,
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(post.Title, post.Content, post.AuthorID)
+	if err != nil {
+		return 0, err
+	}
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return uint64(lastInsertedID), nil
 }
