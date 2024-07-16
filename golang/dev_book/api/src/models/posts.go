c7f19f08c00cf96737c57b5884704fb4e5fd65bd
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-16 18:39:11 -0300

    :sparkles: add validate func

--- golang/dev_book/api/src/models/posts.go
+++ golang/dev_book/api/src/models/posts.go
@@ -1,21 +1,32 @@
 package models
 
 import (
+	"errors"
 	"strings"
 	"time"
 )
 
 type Posts struct {
-	ID uint64 `json:"id,omitempty"`
-	Title string `json:"title,omitempty"`
-	Content string `json:"content,omitempty"`
-	AuthorID uint64 `json:"author_id,omitempty"`
-	AuthorUserName uint64 `json:"author_user_name,omitempty"`
-	Likes uint64 `json:"likes"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
+	ID             uint64    `json:"id,omitempty"`
+	Title          string    `json:"title,omitempty"`
+	Content        string    `json:"content,omitempty"`
+	AuthorID       uint64    `json:"author_id,omitempty"`
+	AuthorUserName uint64    `json:"author_user_name,omitempty"`
+	Likes          uint64    `json:"likes"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
 }
 
 func (post *Posts) format() {
 	post.Title = strings.TrimSpace(post.Title)
 	post.Content = strings.TrimSpace(post.Content)
+}
+
+func (post *Posts) validate() error {
+	if post.Title == "" {
+		return errors.New("field cannot be empty")
+	}
+	if post.Content == "" {
+		return errors.New("field cannot be empty")
+	}
+	return nil
 }
