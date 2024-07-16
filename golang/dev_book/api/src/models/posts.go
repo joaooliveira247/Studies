25cd659f0654cd96d0db5e38bd036df3252c8294
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-16 18:37:18 -0300

    :bug: fix content type

--- golang/dev_book/api/src/models/posts.go
+++ golang/dev_book/api/src/models/posts.go
@@ -1,11 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Posts struct {
 	ID uint64 `json:"id,omitempty"`
 	Title string `json:"title,omitempty"`
-	Content uint64 `json:"content,omitempty"`
+	Content string `json:"content,omitempty"`
 	AuthorID uint64 `json:"author_id,omitempty"`
 	AuthorUserName uint64 `json:"author_user_name,omitempty"`
 	Likes uint64 `json:"likes"`
