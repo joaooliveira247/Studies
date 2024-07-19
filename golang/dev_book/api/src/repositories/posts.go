01e4326e5dd5833589beac2a5730982a45b11988
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-19 10:42:05 -0300

    :sparkles: add Unlike Repository

--- golang/dev_book/api/src/repositories/posts.go
+++ golang/dev_book/api/src/repositories/posts.go
@@ -208,3 +208,29 @@ func (p Posts) LikePost(postID uint64) error {
 	}
 	return nil
 }
+
+func (p Posts) UnlikePost(postID uint64) error {
+	statement, err := p.db.Prepare(
+		`UPDATE 
+			posts 
+		SET
+			likes = 
+				CASE WHEN 
+					likes > 0 
+				THEN
+					likes - 1
+				ELSE
+					likes
+				END
+		WHERE id = ?;`,
+	)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+
+	if _, err = statement.Exec(postID); err != nil {
+		return err
+	}
+	return nil
+}
