e5a4007244bb5fe99711b746ba501972d4b08d85
joaooliveira247 <joaooliveira247@gmail.com>
2024-10-19 19:14:02 -0300

    :sparkles: add terraform block example

---
+++ terraform/terraform_example.tf
@@ -0,0 +1,19 @@
+terraform {
+    // >=, >, <=, <, !=, ~> 1.0.0 - 1.0.n
+  required_version = "~> 1.0.0"
+  required_providers {
+    aws = {
+        version = "1.0"
+        source = "hashicorp/aws"
+    }
+
+    azurerm = {
+        version = "2.0"
+        source = "hashicorp/azurerm"
+    }
+  }
+
+  backend "azurerm" {
+    
+  }
+}
