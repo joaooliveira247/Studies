737a304c66227a24a5156e657ca4f26b3ac42dbf
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-15 19:44:02 -0300

    :sparkles: add jsTypes

---
+++ javascript/basic/jsTypes.js
@@ -0,0 +1,19 @@
+// string
+const name = "some name";
+const name2 = 'some name';
+const name3 = `some name`;
+// number
+const num1 = 10; // int
+const num2 = 15.5; // float
+// undefined
+let studentName; // defined by lang
+const studentLastName = null; // defined by dev
+// bool
+const hasStudentIssue = true;
+const hasStudentApproved = false;
+
+console.log(typeof studentName);
+
+const a = 2;
+
+const b = a; // do a copy of a not a reference
