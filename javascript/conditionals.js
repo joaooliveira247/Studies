dbdd3e16c016efdec4cac3ff498c4a7c0cdec23b
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-17 20:53:03 -0300

    :sparkles: add conditionals

---
+++ javascript/conditionals.js
@@ -0,0 +1,9 @@
+let age = 17;
+
+if (age >= 18) {
+    console.log("Access Granted");
+} else if (age >= 16 && age < 18) {
+    console.log("Access Limited");
+} else {
+    console.log("Access Denied");
+}
