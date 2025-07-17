cde2e3ac38b7085f00a7358c4531b77df7157a15
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-17 19:04:06 -0300

    :sparkles: add basic-operators

---
+++ javascript/basic-operatos.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log(`Sum: ${num1 + num2}, Sub: ${num1 - num2}, Mult: ${num1 * num2}, Div: ${num1 / num2}, Pow: ${num2 ** 2}`);
+
+let numAsString = String(num1);
+
+console.log(numAsString, typeof numAsString, typeof num1);
