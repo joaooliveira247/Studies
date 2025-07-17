d9b977d7f5cf17a0c903c08e8b5803df41f379b7
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-17 20:38:42 -0300

    :sparkles: add relational-logic

---
+++ javascript/relational-logic.js
@@ -0,0 +1,5 @@
+let num1 = 10;
+let num2 = 5;
+let num3 = 10;
+
+console.log(`eq: ${num1 == String(num3)}, strict eq: ${num1 === num3}, dif: ${num2 != num1}, gte: ${num1 >= num2}, lte: ${num1 <= num2}`)
