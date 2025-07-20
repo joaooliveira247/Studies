f69c3d64d660ed6e47d3da31232a38d292e819d0
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-20 17:44:15 -0300

    :sparkles: anon-func-arrow

---
+++ javascript/anon-func-arrow.js
@@ -0,0 +1,15 @@
+// literal
+function sum(a, b) {
+    return a + b;
+}
+
+// anon
+const sum = function (a, b) { return a + b;}
+
+// arrow
+
+const sum = (a, b) => {return a + b;}
+
+// arrow with return impl
+
+const sum = (a, b) => a + b;
