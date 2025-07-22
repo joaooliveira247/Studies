677cd462cf51a9dc283d707009e15b5a5499a508
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-22 17:32:53 -0300

    :sparkles: add math

---
+++ javascript/math.js
@@ -0,0 +1,9 @@
+let num = 280.50;
+
+console.log(Math.PI);
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.round(num));
+console.log(Math.pow(9, 2));
+console.log(Math.random() * 10);
+console.log(Math.sqrt(144));
