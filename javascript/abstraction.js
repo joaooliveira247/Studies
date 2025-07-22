9f137c9d37bff35a6240bc4d12c5439f82cea1dd
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-22 07:26:22 -0300

    :sparkles: add abstraction

---
+++ javascript/abstraction.js
@@ -0,0 +1,10 @@
+class Car{
+    constructor() {
+        this.brand = "Volkswagen",
+        this.model = "Gol",
+        this.color = "Grey",
+        this.sign = "EMJ-2565"
+    }
+}
+
+const car1 = new Car()
