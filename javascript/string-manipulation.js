cef92165b5590a7e17658de56b68b3626905d4a6
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-22 17:29:28 -0300

    :sparkles: add string-manipulation

---
+++ javascript/string-manipulation.js
@@ -0,0 +1,7 @@
+let firstName = "John";
+
+console.log(firstName.length);
+console.log(firstName.charAt(2));
+console.log(firstName.replace("o", "a"));
+console.log(firstName.substring(1, 3));
+console.log(firstName.split("o"));
