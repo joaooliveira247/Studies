3e07618aed573dd07121d3b37f6fa95b66b93166
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-23 06:23:02 -0300

    :sparkles: add more-arrays

---
+++ javascript/more-arrays.js
@@ -0,0 +1,14 @@
+const names = ["John", "Peter"];
+
+const fruits = new Array("Strawberry", "Apple");
+
+names.push("Mariah");
+
+delete names[1];
+
+names[0] = "Joseph";
+
+console.log(fruits.length)
+console.log(fruits)
+fruits.sort()
+console.log(fruits)
