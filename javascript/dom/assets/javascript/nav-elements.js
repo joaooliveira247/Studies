e1ff9142a60cc904d17520b0c505006a60c084a8
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-24 18:00:03 -0300

    :sparkles: add nav-elements

---
+++ javascript/dom/assets/javascript/nav-elements.js
@@ -0,0 +1,12 @@
+const list = document.querySelector('div#itens');
+
+// console.log(list);
+// console.log(list.parentNode);
+// console.log(list.childNodes);
+// console.log(list.firstChild);
+// console.log(list.lastChild);
+// console.log(list.nextSibling);
+// console.log(list.previousSibling);
+console.log(list.firstElementChild);
+
+
