a59ce5d019fa94844e9d63a6afbe5dd6ead84ec6
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-28 11:34:15 -0300

    :sparkles: add read-only-arrays-tuples

---
+++ typescript/basic-types/src/read-only-arrays-tuples.ts
@@ -0,0 +1,10 @@
+// let numbers: readonly number[] = [10, 20, 30, 40, 50];
+let numbers: ReadonlyArray<number> = [10, 20, 30, 40, 50];
+
+// numbers[0] = 70;
+
+console.log(numbers);
+
+let numbersTuple: readonly [number, number] = [1, 2];
+
+export default () => {};
