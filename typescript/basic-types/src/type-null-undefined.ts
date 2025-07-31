ecc9e82f875931a01f916f4dc74f70d082546816
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-31 09:30:25 -0300

    :sparkles: add type-null-undefined

---
+++ typescript/basic-types/src/type-null-undefined.ts
@@ -0,0 +1,9 @@
+export const bootstrap = (): void => {
+    // null - false - typeof(object) - 
+
+    let title = null;
+
+    // undefined - false - typeof(undefined) -
+
+    let subtitle = undefined;
+};
