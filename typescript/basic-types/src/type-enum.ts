01ad2e5f7695187668e7ab00e96ba33c24d4c44e
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-31 09:29:41 -0300

    :sparkles: add type-enum

---
+++ typescript/basic-types/src/type-enum.ts
@@ -0,0 +1,13 @@
+enum OrderStatus {
+    PENDING = "Pendente",
+    DELIVERED = "Entregue",
+    CANCELED = "Cancelado",
+}
+
+console.log(OrderStatus);
+console.log(OrderStatus.DELIVERED);
+// console.log(OrderStatus[1]);
+
+enum OrderStatus {
+    WAITINGFORPAYMENT = "Aguardando por pagamento",
+}
