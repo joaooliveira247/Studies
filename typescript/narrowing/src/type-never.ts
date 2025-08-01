2586458a903171556734c464fd9545817129238a
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-01 08:55:03 -0300

    :sparkles: add type-never

---
+++ typescript/narrowing/src/type-never.ts
@@ -0,0 +1,9 @@
+function throwError(message: string): never {
+    throw new Error(message);
+}
+
+throwError("Aplicação interrompida. Erro interno!");
+console.log("Continue");
+console.log("Continue");
+console.log("Continue");
+console.log("Continue");
