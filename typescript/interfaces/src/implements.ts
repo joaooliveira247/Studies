1cabb1f08c587dc2d6ef9a43dcc5fd2dfc0f90fe
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-01 15:20:34 -0300

    :sparkles: add implements

---
+++ typescript/interfaces/src/implements.ts
@@ -0,0 +1,40 @@
+enum SkillLevel {
+    BEGINNER = "beginner",
+    INTERMEDIATE = "intermediate",
+    ADVANCED = "advanced",
+}
+
+interface Resume {
+    fullName: string;
+    email: string;
+    skills: Skill[];
+    addSkill: (skill: Skill) => void;
+}
+
+interface Skill {
+    name: string;
+    level: SkillLevel;
+}
+
+class MyResume implements Resume {
+    constructor(
+        public fullName: string,
+        public email: string,
+        public skills: Skill[]
+    ) {}
+
+    public addSkill(skill: Skill): void {
+        this.skills.push(skill);
+    }
+}
+
+const myResume: Resume = new MyResume("John", "john@email.com", [
+    { name: "TypeScript", level: SkillLevel.BEGINNER },
+    { name: "Go", level: SkillLevel.INTERMEDIATE },
+    { name: "JavaScript", level: SkillLevel.BEGINNER },
+    { name: "Python", level: SkillLevel.ADVANCED },
+]);
+
+console.log(myResume);
+myResume.addSkill({ name: "Rust", level: SkillLevel.BEGINNER });
+console.log(myResume);
