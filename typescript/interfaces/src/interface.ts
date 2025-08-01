f969adb8c676b4361814d5ecfc6bea9208f11f78
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-01 15:19:44 -0300

    :sparkles: add interfaces

---
+++ typescript/interfaces/src/interface.ts
@@ -0,0 +1,33 @@
+/*
+enum SkillLevel {
+    BEGINNER = "beginner",
+    INTERMEDIATE = "intermediate",
+    ADVANCED = "avanced",
+}
+
+interface Resume {
+    fullName: string;
+    email: string;
+    skills: Skill[];
+    addSkill?: (skill: Skill) => void;
+}
+
+interface Skill {
+    name: string;
+    level: SkillLevel;
+}
+
+const myResume: Resume = {
+    fullName: "John",
+    email: "john@email.com",
+    skills: [
+        { name: "TypeScript", level: SkillLevel.BEGINNER },
+        { name: "Go", level: SkillLevel.INTERMEDIATE },
+        { name: "JavaScript", level: SkillLevel.BEGINNER },
+        { name: "Python", level: SkillLevel.ADVANCED },
+        { name: "Python", level: SkillLevel.ADVANCED },
+    ],
+};
+
+console.log(myResume);
+*/
