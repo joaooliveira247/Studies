ae5907f4c617b1df3023af0d78f318b8583418bf
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-11 06:30:46 -0300

    :sparkles: more about components (integrate ts with react)

---
+++ react/react-fundamentos/src/app/fundamentos/1_integracao/page.tsx
@@ -0,0 +1,9 @@
+export default function Integracao() {
+    const firstName = "john";
+    return (
+        <div>
+            <h1>Integração</h1>
+            <h2>Hello, {firstName}</h2>
+        </div>
+    );
+}
