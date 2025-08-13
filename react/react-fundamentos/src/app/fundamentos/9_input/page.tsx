3bcff2027f06b0ea4c8fcaceb2e4effb706c5b33
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-13 10:25:13 -0300

    :sparkles: add input

---
+++ react/react-fundamentos/src/app/fundamentos/9_input/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Input</h1>
+            <input
+                type="text"
+                className="text-field"
+                value={"Type you first name"}
+            />
+        </div>
+    );
+}
