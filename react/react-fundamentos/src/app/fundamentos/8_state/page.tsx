ff951364179612b7e0f564718dd7cf99ead96625
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-13 09:51:00 -0300

    :sparkles: add components stateless

---
+++ react/react-fundamentos/src/app/fundamentos/8_state/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Page() {
+    let num = 0;
+
+    function increment() {
+        num += 1;
+        console.log(num);
+    }
+
+    return (
+        <div>
+            <h1>States</h1>
+            <button className="bg-blue-400 p-2" onClick={increment}>
+                Click here
+            </button>
+
+            <p>Number: {num}</p>
+        </div>
+    );
+}
