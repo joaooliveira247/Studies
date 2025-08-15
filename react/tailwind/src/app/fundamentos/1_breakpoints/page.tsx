98a17b3761e907505cfc9a78917d5984fe0f445b
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-15 18:53:40 -0300

    :sparkles: add breakpoints

---
+++ react/tailwind/src/app/fundamentos/1_breakpoints/page.tsx
@@ -0,0 +1,11 @@
+/* breakpoints(responsividade)
+sm, md(horizontal), lg - mobile
+xl, 2xl - telas maiores monitor/tv
+*/
+export default function Responsividade() {
+    return (
+        <div>
+            <h1 className="bg-slate-300 md:bg-orange-300 lg:bg-blue-500 xl:bg-amber-800 2xl:bg-green-400"></h1>
+        </div>
+    );
+}
