6149b953e9be577d502f32ef15a77934aedf0484
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-16 15:30:49 -0300

    :sparkles: add tipografia

---
+++ react/tailwind/src/app/fundamentos/2_tipografia/page.tsx
@@ -0,0 +1,11 @@
+export default function Tipografia() {
+    return (
+        <div>
+            <h1 className="font-sans">Title 1</h1>
+            <h1 className="font-serif">Title 2</h1>
+            <h1 className="font-mono">Title 3</h1>
+            <h1 className="text-2xl">Title 4</h1>
+            <h1 className="text-[3rem]">Title 5</h1>
+        </div>
+    );
+}
