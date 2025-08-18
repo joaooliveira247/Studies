dd5e86354520dee8587af64777dee8701a608238
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-18 20:04:10 -0300

    :sparkles: add borders

---
+++ react/tailwind/src/app/fundamentos/2_borders/page.tsx
@@ -0,0 +1,16 @@
+export default function Borders() {
+    return (
+        <div>
+            <div className="m-8 border-2 border-orange-500 rounded-2xl">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+                cumque, enim, facilis eum atque tempore hic voluptates
+                veritatis ratione dolore saepe aliquid fugiat? Fugiat repellat
+                praesentium omnis non obcaecati distinctio!
+            </div>
+
+            <button className="rounded-full bg-blue-500 px-4 text-white">
+                Save Changes
+            </button>
+        </div>
+    );
+}
