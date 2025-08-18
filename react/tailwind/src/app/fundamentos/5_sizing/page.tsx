8f1f98d79ee5aadb3456acb1e4304065ddda7b85
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-18 12:23:02 -0300

    :sparkles: add sizing

---
+++ react/tailwind/src/app/fundamentos/5_sizing/page.tsx
@@ -0,0 +1,19 @@
+export default function Sizing() {
+    return (
+        <div className="flex">
+            {/* w-96, w-[500px], w-2/5, md:w-auto, size-40, h-2, h-full */}
+            <div className="bg-orange-200 w-[500px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Veniam eligendi dolores, accusantium obcaecati omnis quaerat
+                fuga amet asperiores libero. Est perferendis possimus porro
+                numquam quas saepe, corrupti nobis quod beatae.
+            </div>
+            <div className="bg-orange-500 w-[500px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Veniam eligendi dolores, accusantium obcaecati omnis quaerat
+                fuga amet asperiores libero. Est perferendis possimus porro
+                numquam quas saepe, corrupti nobis quod beatae.
+            </div>
+        </div>
+    );
+}
