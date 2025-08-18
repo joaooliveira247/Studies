ea66d9301d07f7c73eb10fe6bc2958ab68cc014d
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-17 21:47:33 -0300

    :sparkles: add espacamentos

---
+++ react/tailwind/src/app/fundamentos/4_espacamentos/page.tsx
@@ -0,0 +1,13 @@
+export default function Espacamentos() {
+    return (
+        <div>
+            {/* p-[16px] md:p-2 lg:p-4*/}
+            <div className="bg-blue-300 p-2">
+                Padding
+            </div>
+            <div className="m-auto">
+                Margin
+            </div>
+        </div>
+    );
+}
