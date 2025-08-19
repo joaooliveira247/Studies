52f69c31d038e88cbf331d4ec84149e297ae58b3
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-19 06:57:01 -0300

    :sparkles: add columns

---
+++ react/tailwind/src/app/fundamentos/7_colums/page.tsx
@@ -0,0 +1,13 @@
+export default function Columns() {
+    return (
+        // <div className="p-2 columns-3 gap-8 ">
+        <div className="p-2 columns-1 md:columns-2 lg:columns-3 ">
+            <div className="size-32 border bg-blue-500">item 1</div>
+            <div className="size-32 border bg-blue-500">item 2</div>
+            <div className="size-32 border bg-blue-500">item 3</div>
+            <div className="size-32 border bg-blue-500">item 4</div>
+            <div className="size-32 border bg-blue-500">item 5</div>
+            <div className="size-32 border bg-blue-500">item 6</div>
+        </div>
+    );
+}
