a28d3bd399af5976ea5d0d6f040144cc2da2f54c
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-19 06:37:20 -0300

    :sparkles: add components

---
+++ react/tailwind/src/app/fundamentos/6_componentes/page.tsx
@@ -0,0 +1,9 @@
+export default function Components() {
+    return (
+        <div className="p-2">
+            <button className="btn-success">Success</button>
+            <button className="btn-alert">Alert</button>
+            <button className="btn-error">Error</button>
+        </div>
+    );
+}
