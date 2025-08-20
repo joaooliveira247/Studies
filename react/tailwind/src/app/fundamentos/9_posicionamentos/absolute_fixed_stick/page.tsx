fafe06f8540457315183c7e598c1976d9a8f15a7
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-20 11:22:15 -0300

    :sparkles: add absolute, fixed e sticky

---
+++ react/tailwind/src/app/fundamentos/9_posicionamentos/absolute_fixed_stick/page.tsx
@@ -0,0 +1,22 @@
+export default function AbsoluteFixedStick() {
+    <div className="border p-3">
+        {/* <p className="bg-orange-500 m-2 absolute bottom-2"> */}
+        {/* <p className="bg-orange-500 m-2 fixed bottom-2"> */}
+        <p className="bg-orange-500 m-2 sticky top-1 z-10">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero.
+        </p>
+        <p className="bg-purple-400 m-2 stick top-2 z-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero. Corrupti non odio veniam autem
+            deserunt esse temporibus, nihil officia reprehenderit! Corporis
+            quis dolores quas reiciendis fugiat amet.
+        </p>
+        <p className="bg-gray-400 m-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero. Corrupti non odio veniam autem
+            deserunt esse temporibus, nihil officia reprehenderit! Corporis
+            quis dolores quas reiciendis fugiat amet.
+        </p>
+    </div>;
+}
