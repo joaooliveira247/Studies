f38ee54b17c7da917a820e945b9b587597937c86
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-20 11:16:02 -0300

    :sparkles: add static_relative

---
+++ react/tailwind/src/app/fundamentos/9_posicionamentos/static_relative/page.tsx
@@ -0,0 +1,20 @@
+export default function StaticRelative() {
+    <div className="border p-3">
+        <p className="bg-orange-500 m-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero.
+        </p>
+        <p className="bg-purple-400 m-2 relative bottom-4 right-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero. Corrupti non odio veniam autem
+            deserunt esse temporibus, nihil officia reprehenderit! Corporis
+            quis dolores quas reiciendis fugiat amet.
+        </p>
+        <p className="bg-gray-400 m-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem earum quam libero. Corrupti non odio veniam autem
+            deserunt esse temporibus, nihil officia reprehenderit! Corporis
+            quis dolores quas reiciendis fugiat amet.
+        </p>
+    </div>;
+}
