a3aa7c668b2f9a62e16195d4b5853f09f2ce754e
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-20 13:52:38 -0300

    :sparkles: add flex directions e espacamentos

---
+++ react/tailwind/src/app/fundamentos/10_flex/direcao_especamentos/page.tsx
@@ -0,0 +1,10 @@
+export default function Application() {
+    return (
+        <div className="flex border p-3 flex-row flex-wrap">
+            <div className="box basis-64">Box 1</div>
+            <div className="box basis-96">Box 2</div>
+            <div className="box">Box 3</div>
+            <div className="box">Box 4</div>
+        </div>
+    );
+}
