ef3ceba2d83c6f3f22525ec0d5ef5a4c913c4f86
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-21 10:38:56 -0300

    :sparkles: add redimentsionamento

---
+++ react/tailwind/src/app/fundamentos/10_flex/redimensionamento/page.tsx
@@ -0,0 +1,9 @@
+export default function Redimensionamento() {
+    return (
+        <div className="border-4 p-3 flex gap-2">
+            <div className="box w-48 grow-0 shrink-0"> Box 1</div>
+            <div className="box w-72 grow shrink"> Box 2 </div>
+            <div className="box w-96 grow-0 shrink-0"> Box 3 </div>
+        </div>
+    );
+}
