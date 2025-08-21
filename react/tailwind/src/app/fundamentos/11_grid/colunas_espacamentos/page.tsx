19991adb0a3af44b06bcea0b3362bcf5b81ec822
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-21 10:48:28 -0300

    :sparkles: add colunas e espacamentos

---
+++ react/tailwind/src/app/fundamentos/11_grid/colunas_espacamentos/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    <div className="border-4 p-4 grid grid-cols-4 gap-2">
+        <div className="box col-start-1 col-end-3"> Box 1</div>
+        <div className="box"> Box 2</div>
+        <div className="box"> Box 3</div>
+        <div className="box"> Box 4</div>
+        <div className="box"> Box 5</div>
+        <div className="box"> Box 6</div>
+        <div className="box"> Box 7</div>
+        <div className="box"> Box 8</div>
+        <div className="box"> Box 9</div>
+    </div>;
+}
