a6064c0283d6f85440d951fc7dac621a1ee398e6
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-21 11:02:02 -0300

    :sparkles: add container

---
+++ react/tailwind/src/app/fundamentos/12_container/page.tsx
@@ -0,0 +1,23 @@
+export default function Page() {
+    return (
+        <div className="container mx-auto bg-green-300 border-3">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+                nobis odit impedit quas vitae quisquam minus delectus laborum,
+                eius fuga similique provident cumque a sunt facilis illo
+                blanditiis eligendi culpa?
+            </p>
+            <div className="border-4 p-3 grid gap-2 grid-cols-4">
+                <div className="box"> Box 1 </div>
+                <div className="box"> Box 2 </div>
+                <div className="box"> Box 3 </div>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                omnis voluptas quasi fugit saepe vitae accusamus eaque
+                molestiae maiores alias numquam dicta distinctio velit id
+                quisquam excepturi, ab consequatur nam?
+            </p>
+        </div>
+    );
+}
