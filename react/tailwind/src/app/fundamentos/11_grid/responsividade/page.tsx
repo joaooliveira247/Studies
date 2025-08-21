120d7b593ba3dd059257b6b49182e3121a8420d4
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-21 10:55:33 -0300

    :sparkles: add responsividade

---
+++ react/tailwind/src/app/fundamentos/11_grid/responsividade/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    <div className="border-4 p-4 grid gap-2 md:grid-cols-2 lg:grid-cols-3">
+        <div className="box col-span-1 md:col-span-2"> Box 1</div>
+        <div className="box"> Box 2</div>
+        <div className="box"> Box 3</div>
+        <div className="box"> Box 4</div>
+        <div className="box"> Box 5</div>
+        <div className="box"> Box 6</div>
+        <div className="box"> Box 7</div>
+        <div className="box"> Box 8</div>
+        <div className="box"> Box 9</div>
+    </div>;
+}
