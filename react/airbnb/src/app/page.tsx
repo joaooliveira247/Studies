fd2a7481cb3182e1e8fbf9b958b26cf97f5657b0
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-28 19:08:10 -0300

    :sparkles: start project airbnb copy

---
+++ react/airbnb/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <div className="border-orange-500 border-2">Header</div>
+            <div className="border-green-500 border-2">Content</div>
+            <div className="border-yellow-500 border-2">Footer</div>
+        </div>
+    );
+}
