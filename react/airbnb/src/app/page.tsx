3581973bae435f47a2a1a4c28873586fe5724b09
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-01 08:00:03 -0300

    :sparkles: add accommodations

--- react/airbnb/src/app/page.tsx
+++ react/airbnb/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Accommodation from "@/widgets/accommodation";
 import Header from "@/widgets/header";
 import NavBar from "@/widgets/NavBar";
 import SearchBar from "@/widgets/SearchBar";
@@ -12,6 +13,7 @@ export default function Home() {
             <hr className="my-3" />
             <main className="container mx-auto">
                 <NavBar />
+                <Accommodation />
             </main>
             <div className="border-green-500 border-2">Content</div>
             <footer className="border-purple-500 border-2">footer</footer>
