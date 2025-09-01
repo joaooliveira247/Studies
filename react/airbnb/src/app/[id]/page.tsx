b33d6df57241cf9c9c079bc6a96e988b06fe5e4c
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-01 10:55:07 -0300

    :sparkles: add base details page

--- react/airbnb/src/app/[id]/page.tsx
+++ react/airbnb/src/app/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Footer from "@/widgets/Footer";
+import Header from "@/widgets/header";
+import SearchBar from "@/widgets/SearchBar";
+
 interface PageProps {
     id: string;
 }
@@ -8,5 +12,18 @@ export default async function Page({
     params: Promise<PageProps>;
 }) {
     const data = await params;
-    return <div>url: {data.id}</div>;
+    return (
+        <div>
+            <header className="container mx-auto">
+                <Header />
+                <SearchBar />
+            </header>
+            <main className="container mx-auto py-6">
+                <h1 className="text-3xl">Rancho da lua</h1>
+            </main>
+            <footer className="bg-gray-200">
+                <Footer />
+            </footer>
+        </div>
+    );
 }
