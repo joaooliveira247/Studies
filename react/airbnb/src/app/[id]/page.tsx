c022ddb3fe206bb5a864bdfe7f727b45d089b410
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-01 10:50:19 -0300

    :sparkles: add how pass params to another page using next

---
+++ react/airbnb/src/app/[id]/page.tsx
@@ -0,0 +1,12 @@
+interface PageProps {
+    id: string;
+}
+
+export default async function Page({
+    params,
+}: {
+    params: Promise<PageProps>;
+}) {
+    const data = await params;
+    return <div>url: {data.id}</div>;
+}
--- react/airbnb/src/widgets/accommodation.tsx
+++ react/airbnb/src/widgets/accommodation.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Accommodation from "@/components/accommodation";
+import Link from "next/link";
 
 const locations = [
     {
@@ -2037,22 +2038,24 @@ const Accommodations = () => {
         <section className="py-6 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {locations.map((location, idx) => (
                 <div key={idx}>
-                    <Accommodation
-                        local={location.location.description}
-                        anfitriao={location.host}
-                        date={location.date}
-                        price={location.price}
-                        avaliacao={location.rating}
-                        preferidoHospede={location.hasBadge}
-                    >
-                        <Image
-                            className="w-full aspect-square object-cover rounded-xl"
-                            src={location.photos[0].source}
-                            alt={location.photos[0].description}
-                            width={300}
-                            height={300}
-                        />
-                    </Accommodation>
+                    <Link href={location.slug}>
+                        <Accommodation
+                            local={location.location.description}
+                            anfitriao={location.host}
+                            date={location.date}
+                            price={location.price}
+                            avaliacao={location.rating}
+                            preferidoHospede={location.hasBadge}
+                        >
+                            <Image
+                                className="w-full aspect-square object-cover rounded-xl"
+                                src={location.photos[0].source}
+                                alt={location.photos[0].description}
+                                width={300}
+                                height={300}
+                            />
+                        </Accommodation>
+                    </Link>
                 </div>
             ))}
         </section>
