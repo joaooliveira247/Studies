3e0c0842a5ac708a41cbe78add51b42be9c7f0f1
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-09 15:01:08 -0300

    :sparkles: add postgres connection and container

--- fullstack/courses_platform/courses_api/core/settings.py
+++ fullstack/courses_platform/courses_api/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,8 +81,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "courses_database",
+        "USER": "user",
+        "PASSWORD": "passwd",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
