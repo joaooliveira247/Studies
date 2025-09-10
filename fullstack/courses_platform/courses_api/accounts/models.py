16a8ab4ec8ba908992c22fce51b2f8c0f9453dcc
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-10 17:50:24 -0300

    :sparkles: add accounts django app

---
+++ fullstack/courses_platform/courses_api/accounts/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_superuser(self, email, password):
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=80)
+    email = models.EmailField(unique=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
+
+    class Meta:
+        db_table = "users"
--- fullstack/courses_platform/courses_api/core/settings.py
+++ fullstack/courses_platform/courses_api/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
     "rest_framework",
 ]
 
@@ -109,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 
 # Internationalization
