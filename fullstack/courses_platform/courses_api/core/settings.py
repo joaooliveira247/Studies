fe19c20fbf772d6953b3425869cfdf4f74c2c085
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-11 08:36:52 -0300

    :sparkles: fix import string call

--- fullstack/courses_platform/courses_api/core/settings.py
+++ fullstack/courses_platform/courses_api/core/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Rest Framework Settings
 REST_FRAMEWORK = {
-    "EXCEPTION_HANDLER": "core.utils.custom_exception.custom_exception_handler",
+    "EXCEPTION_HANDLER": "core.utils.custom_exceptions.custom_exception_handler",
     "DEFAULT_RENDERER_CLASSES": ["core.utils.renderers.CustomJsonRenderer"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
