1ad2e53b37e92539f5c6d1035fee80ede76770e7
joaooliveira247 <joaooliveira247@gmail.com>
2025-12-13 18:42:35 -0300

    :sparkles: add about deref and drop

--- rust/smart_pointers/src/main.rs
+++ rust/smart_pointers/src/main.rs
@@ -1,14 +1,83 @@
 use List::{Cons, Nil};
+use std::mem::drop;
+use std::ops::Deref;
 
+#[allow(dead_code)]
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
-fn main() {
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data ´{}´!", self.data);
+    }
+}
+
+#[allow(dead_code)]
+fn box_example() {
     // let b = Box::new(5);
     // println!("b = {}", b)
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list = {:#?}", list)
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name)
+}
+
+fn main() {
+    /*
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    */
+
+    // let x = 5;
+    // let y = Box::new(x);
+
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+    /*
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    */
+    /*
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    */
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created");
+    drop(c);
 }
