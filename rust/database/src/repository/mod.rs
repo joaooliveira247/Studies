59cf348a155e44705ae63c8eaee937278dd75747
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-16 20:26:19 -0300

    :sparkles: add repository files

---
+++ rust/database/src/repository/mod.rs
@@ -0,0 +1,4 @@
+pub mod user_repository;
+
+pub use user_repository::BookRepository;
+pub use user_repository::BookRepositoryImpl;
---
+++ rust/database/src/repository/user_repository.rs
@@ -0,0 +1,45 @@
+use crate::models::{BookActiveModel, BookModel};
+use anyhow::Ok;
+use async_trait::async_trait;
+use sea_orm::{ActiveModelTrait, ActiveValue::Set, DatabaseConnection};
+
+#[async_trait]
+pub trait BookRepository: Send + Sync {
+    async fn create(
+        &self,
+        title: &str,
+        author: &str,
+        publication_year: i32,
+    ) -> Result<BookModel, anyhow::Error>;
+}
+
+pub struct BookRepositoryImpl {
+    db: DatabaseConnection,
+}
+
+impl BookRepositoryImpl {
+    pub fn new(db: DatabaseConnection) -> Self {
+        Self { db }
+    }
+}
+
+#[async_trait]
+impl BookRepository for BookRepositoryImpl {
+    async fn create(
+        &self,
+        title: &str,
+        author: &str,
+        publication_year: i32,
+    ) -> anyhow::Result<BookModel> {
+        let new_book = BookActiveModel {
+            title: Set(title.to_string()),
+            author: Set(author.to_string()),
+            publication_year: Set(publication_year),
+            ..Default::default()
+        };
+
+        let inserted = new_book.insert(&self.db).await?;
+
+        Ok(inserted)
+    }
+}
