cec517737e7b07255c02eccec23e2b28eb81f44d
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-16 20:27:38 -0300

    :sparkles: add main file

---
+++ rust/database/src/main.rs
@@ -0,0 +1,18 @@
+use database::{config, db, state::AppState};
+
+#[tokio::main]
+async fn main() {
+    config::load_env();
+
+    let db = db::connect().await;
+
+    let state = AppState::new(db.clone());
+
+    let saved = state
+        .book_repo
+        .create("It", "Stephen King", 1986)
+        .await
+        .expect("Error when try create a book.");
+
+    println!("Created book: {:?}", saved)
+}
