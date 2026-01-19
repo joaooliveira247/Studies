7306293bfa64ed942c7ba2370193c38c996217c3
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-18 21:37:26 -0300

    :sparkles: use async get_by_id func

--- rust/database/src/main.rs
+++ rust/database/src/main.rs
@@ -1,4 +1,5 @@
 use database::{config, db, state::AppState};
+use uuid::uuid;
 
 #[tokio::main]
 async fn main() {
@@ -18,6 +19,7 @@ async fn main() {
     println!("Created book: {:?}", saved)
     */
 
+    /*
     let books = state
         .book_repo
         .get_all()
@@ -25,4 +27,16 @@ async fn main() {
         .expect("Error when try find books");
 
     println!("Books: {:?}", books)
+    */
+
+    if let Some(book) = state
+        .book_repo
+        .get_by_id(uuid!("95c431c1-ca73-4fa6-b894-1012d4e263b7"))
+        .await
+        .expect("Error when try find book")
+    {
+        println!("Book: {:?}", book)
+    } else {
+        println!("Book not found")
+    }
 }
