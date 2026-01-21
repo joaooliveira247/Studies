4b1abee11328e097d69b38b8fb5a536f3df69a60
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-21 19:07:53 -0300

    :sparkles: use async delete func

--- rust/database/src/main.rs
+++ rust/database/src/main.rs
@@ -16,18 +16,16 @@ async fn main() {
         .await
         .expect("Error when try create a book.");
 
-    println!("Created book: {:?}", saved)
+    println!("Created book: {:?}", saved);
     */
 
-    /*
-    let books = state
-        .book_repo
-        .get_all()
-        .await
-        .expect("Error when try find books");
+    // let books = state
+    //     .book_repo
+    //     .get_all()
+    //     .await
+    //     .expect("Error when try find books");
 
-    println!("Books: {:?}", books)
-    */
+    // println!("Books: {:?}", books)
 
     /*
     if let Some(book) = state
@@ -41,7 +39,13 @@ async fn main() {
         println!("Book not found")
     }
     */
-    
+    /*
     state.book_repo.update(uuid!("80ba8e52-abb5-4153-8f0b-9fb8669f988d"), Some("Carrie"), None, Some(1974)).await.expect("Error when try update book");
-    
+    */
+
+    let _ = state
+        .book_repo
+        .delete(uuid!("da9055cd-8c8b-4cd5-aae9-a677a72e8006"))
+        .await
+        .expect("Error when try delete book");
 }
