999e3c5d723d54a820650a8ef04ac17089351299
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-27 20:49:35 -0300

    :sparkles: add first example of api

---
+++ rust/api/src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, http::StatusCode, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn root() -> &'static str {
+    "Hello, Axum!"
+}
