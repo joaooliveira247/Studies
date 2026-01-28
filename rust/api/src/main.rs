7ed49fdb5378a86c39861b39fbc08bc75cb5b556
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-28 11:47:20 -0300

    :sparkles: finish example

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{Router, routing::get};
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
 use tower_http::trace::TraceLayer;
 
 #[tokio::main]
@@ -7,6 +12,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
+        .route("/user/", post(create_user))
         .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -18,4 +24,24 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, Axum!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1137,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
