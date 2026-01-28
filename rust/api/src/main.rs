cc397c2f858e952552f07c3f8453234babe1e8f6
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-28 11:11:49 -0300

    :sparkles: add log

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -1,10 +1,17 @@
-use axum::{Router, http::StatusCode, routing::get};
+use axum::{Router, routing::get};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    tracing::info!("🚀 Server running on http://localhost:3000");
 
     axum::serve(listener, app).await.unwrap()
 }
