ec6cae5a4702991477a938e637f339e69528c72e
joaooliveira247 <joaooliveira247@gmail.com>
2026-01-28 15:04:31 -0300

    :sparkles: add unsafe_rust

---
+++ rust/unsafe_rust/src/main.rs
@@ -0,0 +1,22 @@
+#[allow(dead_code)]
+fn raw_pointers() {
+    let mut num = 5;
+
+    let r1 = &raw const num;
+    let r2 = &raw mut num;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+}
+
+unsafe extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+fn main() {
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+}
