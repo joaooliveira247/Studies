2718cb09afea14cc745a5088beb489e72a200483
joaooliveira247 <joaooliveira247@gmail.com>
2026-02-25 18:25:43 -0300

    :sparkles: add consumer

---
+++ go_rabbitmq/consumer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ Teste")
+
+	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully Connected To RabbitMQ instance.")
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+	
+	msgs, err := ch.Consume(
+		"TestQueue",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	
+	forever := make(chan bool)
+	
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Recieved Message: %s\n", d.Body)
+		}
+	}()
+	
+	fmt.Println("Successfully connectec to our RabbitMQ instance")
+	fmt.Println("[*] - waiting for messages")
+	<-forever
+}
