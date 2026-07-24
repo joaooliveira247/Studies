214cbb4cacd98df307dc64a4653a731313f28b41
joaooliveira247 <joaooliveira247@gmail.com>
2026-07-24 18:53:18 -0300

    :sparkles: add palindrome

---
+++ golang/dsa/palindrome/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func isPalindrome(word string) bool {
+	start := 0
+	end := len(word) - 1
+
+	for start < end {
+		letterStart := fmt.Sprintf("%c", word[start])
+		letterEnd := fmt.Sprintf("%c", word[end])
+
+		if letterStart != letterEnd {
+			return false
+		}
+
+		start++
+		end--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println("ARARA", isPalindrome("ARARA"))
+}
