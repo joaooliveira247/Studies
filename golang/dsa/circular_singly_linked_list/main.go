a1bf7da4df51de11935d1305c1fa46aa991702b4
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-06 14:22:39 -0300

    :sparkles: add insertNodeAtEnd

--- golang/dsa/circular_singly_linked_list/main.go
+++ golang/dsa/circular_singly_linked_list/main.go
@@ -46,6 +46,19 @@ func (cl *CircularLinkedList) insertNodeAtBeginnig(data string) {
 	cl.Length++
 }
 
+func (cl *CircularLinkedList) insertNodeAtEnd(data string) {
+	newNode := &ListNode{Data: data}
+
+	if cl.Last == nil || cl.Length == 0 {
+		cl.Last = newNode
+		cl.Last.Next = cl.Last
+	} else {
+		newNode.Next, cl.Last.Next = cl.Last.Next, newNode
+		cl.Last = newNode
+	}
+	cl.Length++
+}
+
 func main() {
 	cl := &CircularLinkedList{}
 	cl.insertNodeAtBeginnig("X")
