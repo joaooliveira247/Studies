58efaad4c576d8deee66368e34a49ecfb8fe58ad
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-06 13:55:25 -0300

    :sparkles: add String method in CircularLinkedList struct

--- golang/dsa/circular_singly_linked_list/main.go
+++ golang/dsa/circular_singly_linked_list/main.go
@@ -12,6 +12,21 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func (cl *CircularLinkedList) String() string {
+	if cl.Length == 0 {
+		return ""
+	}
+
+	var agg []string
+
+	first := cl.Last.Next
+	for first != cl.Last {
+		agg = append(agg, first.Data)
+		first = first.Next
+	}
+	return strings.Join(agg, " --> ")
+}
+
 func main() {
 	cl := &CircularLinkedList{}
 }
